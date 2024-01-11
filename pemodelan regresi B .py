82af902ccb460a85802bca9412f6efdc5ae028e4
noralizaaa <133139847+noralizaaa@users.noreply.github.com>
2024-01-11 19:59:51 +0700

    Update pemodelan regresi B .py

--- pemodelan regresi B .py
+++ pemodelan regresi B .py
@@ -1,15 +1,42 @@
 import pandas as pd
+import statsmodels.api as sm
 import numpy as np
 
-data = pd.read_csv('Cardiovascular Disease Dataset.csv')
+# Membaca data dari file CSV
+data = pd.read_csv ('Cardiovascular Disease Dataset.csv')
 
-# Ambil 100 data
-sample_data = data.sample(1000)
+# Handling Missing Values
+data = data.fillna(data.mean())
 
-# Ambil variabel numerik
-X = sample_data[['age','gender','chestpain','restingBP', 'serumcholestrol','fastingbloodsugar','restingrelectro','maxheartrate','exerciseangia','oldpeak']]
-Y = sample_data['target']
+# Handling Infinity
+data.replace([np.inf, -np.inf], np.nan, inplace=True)
+data = data.fillna(data.max())
 
-# Hitung koefisien regresi menggunakan numpy
-coefficients = np.linalg.inv(X.T @ X) @ X.T @ Y
-print("Koefisien regresi:\n", coefficients)
+# Memilih 5 kolom numerik sebagai variabel independen
+independent_variables = ['age','gender','chestpain','restingBP','serumcholestrol', 'fastingbloodsugar', 'restingrelectro', 'maxheartrate', 'exerciseangia', 'oldpeak']
+
+# Menambahkan konstanta untuk model regresi
+X = sm.add_constant(data[independent_variables])
+
+# Membuat model regresi
+model = sm.OLS(data['target'], X)
+
+# Menyesuaikan model
+results = model.fit()
+
+# Menampilkan hasil regresi
+print(results.summary())
+
+# Fungsi untuk pemodelan regresi
+def predict_regression(model, data):
+    X_pred = sm.add_constant(data[independent_variables])
+    predictions = model.predict(X_pred)
+    return predictions
+
+# Menggunakan seluruh baris data untuk prediksi
+predictions = predict_regression(results, data)
+data['Predicted_target'] = predictions
+
+# Menampilkan hasil prediksi
+print("\nPredictions for all data:")
+print(data[['age', 'gender', 'chestpain', 'restingBP', 'serumcholestrol', 'fastingbloodsugar', 'restingrelectro', 'maxheartrate', 'exerciseangia', 'oldpeak', 'slope', 'noofmajorvessels','Predicted_target']])
